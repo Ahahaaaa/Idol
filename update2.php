3659c1d47a6e0fca49dcc1395dc599ce3aea8d7d
kaede sato <ksw2170057@stu.o-hara.ac.jp>
2022-06-16 16:46:48 +0900

    cc

--- update2.php
+++ update2.php
@@ -1,3 +1,37 @@
+<?php 
+    $id = $_GET['id'];
+    $user='postgres';
+$password ='Hara1215';
+$dbName='postgres';
+$host='localhost';
+
+$name = $_POST['name'];
+$birth = $_POST['birth'];
+$age= $_POST['age'];
+$from = $_POST['from'];
+
+    try {
+        $dsn="pgsql:host={$host};dbname={$dbName}";
+       
+        $pdo = new PDO($dsn, $user, $password);
+    } catch (PDOException $e) {
+        die("接続に失敗しました" . $e->getMessage());
+    }
+    $sql = UPDATE aidle SET  = (値1), (カラム名2) = (値2) WHERE (条件);
+    if ($stmt = $pdo->prepare($sql)) {
+        //$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+        //$stmt->bindValue(':birth', $birth, PDO::PARAM_INT);
+        //$stmt->bindValue(':age', $age, PDO::PARAM_INT);
+        //$stmt->bindValue(':from', $from, PDO::PARAM_STR);
+        $stmt->execute();
+    }
+
+    $pdo = null;
+?>
+<form action="test.php" method="post">
+		<input type="submit" value="戻る">
+		</form>
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +42,8 @@
 </head>
 <body>
 <form method="post" action="update3.php">
+     $_GET['ID'];
+
 <ul>
       <li>
         <label>名前：<br>
